a399ea95b541b74ffb47438589e95ab365034b16
Duongbgbg123 <89895234+Duongbgbg123@users.noreply.github.com>
2022-04-24 03:09:11 +0700

    Revert "design_layout" (#2)
    test

--- src/App.js
+++ src/App.js
@@ -1,91 +1,23 @@
-import React from 'react';
-import './App.scss';
+import logo from './logo.svg';
+import './App.css';
+
 function App() {
   return (
     <div className="App">
-      <nav className="navbar app">App bar</nav>
-      <nav className="navbar board">board bar</nav>
-      <div className="board-columns">
-        <div className="column">
-          <header>BrainStorm</header>
-          <ul>
-            <li>
-              <img
-                src="https://tino.org/wp-content/uploads/2021/09/word-image-36-768x384.png"
-                alt="design-img"
-              />
-              design & research
-            </li>
-            <li>sssss</li>
-          </ul>
-          <footer>Add another card</footer>
-        </div>
-        <div className="column">
-          <header>BrainStorm</header>
-          <ul>
-            <li>
-              <img
-                src="https://tino.org/wp-content/uploads/2021/09/word-image-36-768x384.png"
-                alt="design-img"
-              />
-              design & research
-            </li>
-            <li>sssss</li>
-            <li>third</li>
-            <li>sssss</li>
-            <li>third</li>
-            <li>sssss</li>
-            <li>third</li>
-            <li>sssss</li>
-            <li>third</li>
-            <li>sssss</li>
-            <li>third</li>
-          </ul>
-          <footer>Add another card</footer>
-        </div>
-        <div className="column">
-          <header>BrainStorm</header>
-          <ul>
-            <li>
-              <img
-                src="https://tino.org/wp-content/uploads/2021/09/word-image-36-768x384.png"
-                alt="design-img"
-              />
-              design & research
-            </li>
-            <li>sssss</li>
-          </ul>
-          <footer>Add another card</footer>
-        </div>
-        <div className="column">
-          <header>BrainStorm</header>
-          <ul>
-            <li>
-              <img
-                src="https://tino.org/wp-content/uploads/2021/09/word-image-36-768x384.png"
-                alt="design-img"
-              />
-              design & research
-            </li>
-            <li>sssss</li>
-          </ul>
-          <footer>Add another card</footer>
-        </div>
-        <div className="column">
-          <header>BrainStorm</header>
-          <ul>
-            <li>
-              <img
-                src="https://tino.org/wp-content/uploads/2021/09/word-image-36-768x384.png"
-                alt="design-img"
-              />
-              design & research
-            </li>
-            <li>sssss</li>
-          </ul>
-          <footer>Add another card</footer>
-        </div>
-      </div>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
