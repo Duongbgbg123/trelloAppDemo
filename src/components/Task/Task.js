1645c80ecd80695fe09054ce0da442f39cf476b4
duongdo671999@gmail.com <duongdo671999>
2022-04-25 00:20:31 +0700

    restructor

---
+++ src/components/Task/Task.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Task.scss';
+function Task() {
+  return (
+    <>
+      <li className="task-item">
+        <img
+          src="https://tino.org/wp-content/uploads/2021/09/word-image-36-768x384.png"
+          alt="design-img"
+        />
+        design & research
+      </li>
+    </>
+  );
+}
+
+export default Task;
